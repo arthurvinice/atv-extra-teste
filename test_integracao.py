a08f50bb1876a1a6591b19ea634493d74e2b80e8
Arthur <arthurvinice@gmail.com>
2025-06-27 15:29:06 -0300

    corrigindo nomes das funções

--- test_integracao.py
+++ test_integracao.py
@@ -7,13 +7,13 @@ def test_fluxo_cadastro_e_consulta():
     cadastro_produto.usuarios_db.clear()
 
     # Cadastra um usuário
-    assert cadastro_produto.cadastrar_usuario(1, "João")
+    assert cadastro_produto.cadastrar_produto(1, "João")
 
     # Consulta o mesmo usuário
-    usuario = consulta_produto.consultar_usuario(1)
+    usuario = consulta_produto.consultar_produto(1)
     assert usuario == "João", f"Esperado 'João', obtido {usuario}"
 
     # Consulta um usuário inexistente
-    assert consulta_produto.consultar_usuario(2) is None
+    assert consulta_produto.consultar_produto(2) is None
 
 test_fluxo_cadastro_e_consulta()
