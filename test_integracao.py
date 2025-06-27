251e0742d581350aa9f3cc890cc29a3a6b7e27f0
Arthur <arthurvinice@gmail.com>
2025-06-27 15:34:52 -0300

    corrigido

--- test_integracao.py
+++ test_integracao.py
@@ -6,14 +6,14 @@ def test_fluxo_cadastro_e_consulta():
     # Limpa base de dados
     cadastro_produto.produtos_db.clear()
 
-    # Cadastra um usuário
-    assert cadastro_produto.cadastrar_produto(1, "João")
+    # Cadastra um produto
+    assert cadastro_produto.cadastrar_produto(1, "Iphone", 20) is True
 
-    # Consulta o mesmo usuário
-    usuario = consulta_produto.consultar_produto(1)
-    assert usuario == "João", f"Esperado 'João', obtido {usuario}"
+    # Consulta o mesmo produto
+    produto = consulta_produto.consultar_produto(1)
+    assert produto["nome"] == "Iphone", f"Esperado 'Iphone', obtido {produto}"
 
-    # Consulta um usuário inexistente
+    # Consulta um produto inexistente
     assert consulta_produto.consultar_produto(2) is None
 
 test_fluxo_cadastro_e_consulta()
